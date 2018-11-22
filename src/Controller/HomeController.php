e7187113a3b4a681d4c39e2987ce48da00db1f2b
Tadas Kundelis <kundelist@gmail.com>
2018-11-22 21:44:58 +0200

    initial commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
@@ -12,8 +13,9 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $teams = json_decode(file_get_contents(__DIR__.'/../../public/data.json'), true);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'teams' => $teams
         ]);
     }
 }
